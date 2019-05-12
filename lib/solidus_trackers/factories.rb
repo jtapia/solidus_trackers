f7fb93f918efa5a1813d6109114d8d2c95f3778d
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2019-05-11 23:44:12 -0500

    Update factory syntax

--- lib/solidus_trackers/factories.rb
+++ lib/solidus_trackers/factories.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tracker, class: Spree::Tracker do
-    name 'Google Analytics'
-    analytics_id 'A100'
-    active true
-    script '<script></script>'
+    name { 'Google Analytics' }
+    analytics_id { 'A100' }
+    active { true }
+    script { '<script></script>' }
   end
 end
